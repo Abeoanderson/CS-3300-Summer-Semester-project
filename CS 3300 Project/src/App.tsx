f968610f0ca3837a83385143af5a7cf48c7f023a
Abeoanderson <abeoanderson@gmail.com>
2025-06-23 17:06:11 -0600

    add server.js for simple back end and data base

--- CS 3300 Project/src/App.tsx
+++ CS 3300 Project/src/App.tsx
@@ -1,12 +1,43 @@
-import { Routes, Route } from 'react-router-dom'
-import Home from './Pages/home'
-import About from './Pages/about'
-import NotFound from './Pages/notFound'
-import Navbar from './components/Navbar'
-import Login from './Pages/login'
-import Signup from './Pages/signup'
+import { Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Home from "./Pages/home";
+import About from "./Pages/about";
+import NotFound from "./Pages/notFound";
+import Navbar from "./components/Navbar";
+import Login from "./Pages/login";
+import Signup from "./Pages/signup";
+
+type Meal = {
+  id: number;
+  name: string;
+  calories: number;
+};
 
 function App() {
+  const [meals, setMeals] = useState<Meal[]>([]);
+  const [newMeal, setNewMeal] = useState({ name: "", calories: "" });
+
+  useEffect(() => {
+    fetch("http://localhost:3001/meals")
+      .then((res) => res.json())
+      .then(setMeals);
+  }, []);
+  const handleSubmit = async () => {
+    await fetch("http://localhost:3001/meals", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        name: newMeal.name,
+        calories: Number(newMeal.calories),
+      }),
+    });
+
+    const updated = await fetch("http://localhost:3001/meals").then((res) =>
+      res.json()
+    );
+    setMeals(updated);
+    setNewMeal({ name: "", calories: "" });
+  };
   return (
     <>
       <Navbar />
@@ -18,8 +49,7 @@ function App() {
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
-  )
+  );
 }
 
 export default App;
-
